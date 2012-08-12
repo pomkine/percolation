26290500ca282c9f0f4c5ccf0e0f8bfac60d916f
pomkine <pomkine@gmail.com>
2012-08-12 20:40:06 +0400

    blank classes

---
+++ Percolation.java
@@ -0,0 +1,8 @@
+public class Percolation {
+    
+    public Percolation(int N){}
+    public void open(int i, int j){}
+    public boolean isOpen(int i,int j){}
+    public boolean isFull(int i,int j){}
+    public boolean percolates(){}
+}
